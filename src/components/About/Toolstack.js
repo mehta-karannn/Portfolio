503a2a50c009b0935bfbb4c66fb6d68562d6c076
Karan Mehta <karanmehtawork9@gmail.com>
2024-09-13 00:43:46 +0530

    Tech Stack

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -3,16 +3,17 @@ import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
   SiPostman,
-  SiSlack,
+  
   SiVercel,
-  SiMacos,
+  
+  SiWindows
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+        <SiWindows />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
@@ -20,9 +21,7 @@ function Toolstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col>
+    
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
       </Col>
