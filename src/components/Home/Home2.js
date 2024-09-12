e56067202e96df45d8b569d8f093746beb79f57c
Karan Mehta <karanmehtawork9@gmail.com>
2024-09-13 00:57:19 +0530

    Updated Aboutme section

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,21 +19,21 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
+              I fell in love with programming & analysis and I have at least learnt
               something, I think… 🤷‍♂️
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Python , Java , SQL , Javascript </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">Web Technologies and Analytical Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Blockchain.
+                  Data Science and Machine Learning.
                 </b>
               </i>
               <br />
