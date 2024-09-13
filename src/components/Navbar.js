4a49283ef9f4eed622bd6e431ccdaf2ddbab233b
Karan Mehta <karanmehtawork9@gmail.com>
2024-09-13 22:14:08 +0530

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -92,7 +92,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 href="/experience"
                 target="_blank"
@@ -100,7 +100,7 @@ function NavBar() {
               >
                 <ImBlog style={{ marginBottom: "2px" }} /> Experience
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
 
             <Nav.Item className="fork-btn">
               <Button
